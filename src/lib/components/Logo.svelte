47e314474ff388393fa61e93350e4546ac319211
it231505 <it231505@fhstp.ac.at>
2025-05-12 10:13:04 +0200

    add basic navigation

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let props = $props();
+</script>
+
+<img class="logo" src="/img/logo.svg" alt="logo" {...props}>
+
+<style>
+    .logo {
+        width: 100%;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,52 @@
 <script lang="ts">
+	import { Menu } from '@lucide/svelte';
+	import Logo from '$lib/components/Logo.svelte';
+
 	let { children } = $props();
+	let showMenu = $state(false);
+
+	function onclick() {
+		showMenu = !showMenu;
+	}
 </script>
 
 <nav>
-	<a href="/">Home</a>
-    <a href="/portfolio">Portfolio</a>
-	<a href="/about-me">About Me</a>
-	<a href="/contact">Contact</a>
+	<div id="menu-bar">
+		<a href="/">
+			<div id="nav-logo">
+				<Logo />
+			</div>
+		</a>
+		<button {onclick}>
+			<Menu />
+		</button>
+	</div>
+	{#if showMenu}
+	<ul id="main-menu">
+		<a href="/portfolio">Portfolio</a>
+		<a href="/about-me">About Me</a>
+		<a href="/contact">Contact</a>
+	</ul>
+	{/if}
 </nav>
 
 {@render children()}
+
+<style>
+	#nav-logo {
+		width: 2rem;
+	}
+
+	#menu-bar {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+
+	#main-menu {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+</style>
